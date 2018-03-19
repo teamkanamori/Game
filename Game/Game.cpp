aa5c626a22f60f153496acd5bc85baae6866d5b6
kanamori taiki <kbc17b05@stu.kawahara.ac.jp>
2018-03-19 16:13:57 +0900

    kickoff

---
+++ Game/Game.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "Game.h"
+
+
+Game::Game()
+{
+}
+
+
+Game::~Game()
+{
+}
+bool Game::Start()
+{
+	//ƒJƒƒ‰‚ğİ’èB
+	MainCamera().SetTarget({ 0.0f, 10.0f, 0.0f });
+	MainCamera().SetNear(0.1f);
+	MainCamera().SetFar(100.0f);
+	MainCamera().SetPosition({ 30.0f, 10.0f, 0.0f });
+	MainCamera().Update();
+	
+	m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
+	m_skinModelRender->Init(L"modelData/unityChan.cmo");
+	m_skinModelRender->SetScale({ 0.1f, 0.1f, 0.1f } );
+	return true;
+}
+void Game::Update()
+{
+}
+void Game::Render(CRenderContext& rc)
+{
+}
---
+++ Game/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "tkEngine/graphics/effect/tkEffect.h"
+
+class Game : public IGameObject
+{
+public:
+	Game();
+	~Game();
+	bool Start();
+	void Update();
+	void Render(CRenderContext& rc);
+	prefab::CSkinModelRender* m_skinModelRender = nullptr;	//スキンモデルレンダラー。
+	CAnimationClip m_animClip[1];							//アニメーションクリップ。
+	CAnimation m_animation;
+	CVector3 m_pos = CVector3::Zero;
+};
+
