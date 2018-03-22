52befd565b83432a47f9a43250f7b1fb2ed08285
murakami <t>
2018-03-22 12:06:45 +0900

    hoge

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "Game.h"
-#include "testscene.h"
+//#include "testscene.h"
 
 
 Game::Game()
@@ -20,22 +20,22 @@ bool Game::Start()
 	
 
 	////ƒJƒƒ‰‚ğİ’èB
-	//MainCamera().SetTarget({ 0.0f, 10.0f, 0.0f });
-	//MainCamera().SetNear(0.1f);
-	//MainCamera().SetFar(100.0f);
-	//MainCamera().SetPosition({ 30.0f, 10.0f, 0.0f });
-	//MainCamera().Update();
-	//
-	// m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
-	//m_skinModelRender->Init(L"modelData/unityChan.cmo");
-	//m_skinModelRender->SetScale({ 0.1f, 0.1f, 0.1f } );
+	MainCamera().SetTarget({ 0.0f, 10.0f, 0.0f });
+	MainCamera().SetNear(0.1f);
+	MainCamera().SetFar(100.0f);
+	MainCamera().SetPosition({ 30.0f, 10.0f, 0.0f });
+	MainCamera().Update();
+	
+	 m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
+	m_skinModelRender->Init(L"modelData/kokeruhito.cmo");
+	m_skinModelRender->SetScale({ 0.1f, 0.1f, 0.1f } );
 	return true;
 }
 void Game::Update()
 {
-	if (Pad(0).IsPress(enButtonSelect)) {
+	/*if (Pad(0).IsPress(enButtonSelect)) {
 		NewGO<testscene>(0, "testscene");
-	}
+	}*/
 }
 void Game::Render(CRenderContext& rc)
 {
