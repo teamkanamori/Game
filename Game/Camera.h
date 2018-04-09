a36ee1047ba0050a26cf05a424eaa120717d9b89
murakami <t>
2018-04-09 14:43:10 +0900

    hoge

---
+++ Game/Camera.h
@@ -0,0 +1,18 @@
+#pragma once
+class Camera 
+{
+public:
+	Camera();
+	~Camera();
+	bool Start();
+	void Update();
+	void Render(CRenderContext& rc);
+
+	float time = 0;
+	float timestanp[3];
+	int shattercount = 0;
+	CRenderTarget renderTargets[3];
+	CSprite m_sprite;	//シーンをペタッと張り付けるためのスプライト
+	int flag = 0;
+};
+
