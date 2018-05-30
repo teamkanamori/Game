63bc010e88d4f2e3c1f4a7f59b9769ea76d7c3ee
kanamori taiki <kbc17b05@stu.kawahara.ac.jp>
2018-05-30 18:21:25 +0900

    hoge

---
+++ Game/GameCamera.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "tkEngine/camera/tkSpringCamera.h"
+
+class GameCamera : public IGameObject
+{
+public:
+	GameCamera();
+	~GameCamera();
+	bool Start();
+	void Update();
+	/////////////////////////////////////
+	//ƒƒ“ƒo•Ï”
+	/////////////////////////////////////
+};
+
